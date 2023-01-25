b7d3cbb704e04f2212db4f97fd5e5ef7e4ee4471
Steve <stevedsimkins@gmail.com>
2023-01-25 09:57:30 -0500

    fixed pinList

--- pinList.js
+++ pinList.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 
 const pinList = async () => {
   try {
-    const res = await axios.get('https://api.pinata.cloud/data/pinList?includesCount=false&status=pinned', {
+    const res = await axios.get('https://api.pinata.cloud/data/pinList?includeCount=false&status=pinned', {
       headers: {
         'Authorization': `Bearer ${process.env.PINATA_JWT}`
       }
